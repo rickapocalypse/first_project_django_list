6acaeb63be3212499e3f3cf618f0c58440ecff5b
Henrique Apocalypse <rick.apocalypse@hotmail.com>
2022-07-18 20:23:56 -0300

    implementação da funcao delete e edit

--- mysite/tasks/urls.py
+++ mysite/tasks/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('helloworld/', views.helloWorld),
+    # path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
     path('task/<int:id>/', views.taskView, name='task-view'),
     path('newtask/', views.newTask, name='task-new'),
-    path('yourname/<str:name>', views.yourName, name='your-name')
+    path('yourname/<str:name>', views.yourName, name='your-name'),
+    path('edit/<int:id>/', views.editTask, name='task-edit'),
+    path('delete/<int:id>/', views.deleteTask, name='task-delete'),
 ]
--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -1,18 +1,55 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.core.paginator import Paginator
 from django.http import HttpResponse
-
+from .forms import TaskForm
 from .models import Task
+from django.contrib import messages
+
 
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 3)
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page) 
+
     return render(request, 'tasks/list.html', {'tasks':tasks})
 
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
 
-def helloWorld(request):
-    return HttpResponse('Hello World!')
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+
 
 def yourName(request, name):
     return render(request, 'tasks/yourname.html', {'name': name})
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+    messages.info(request, 'Task deleted!')
+    return redirect('/')

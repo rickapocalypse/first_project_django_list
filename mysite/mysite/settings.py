74d1b0b2d0ed20b0a5586aed1e95552a9e80601a
Henrique Apocalypse <rick.apocalypse@hotmail.com>
2022-07-21 20:44:06 -0300

    att login e logout de usuario

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from telnetlib import LOGOUT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'tasks',
     'about',
     'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,3 +142,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
 from .models import Task
 from django.contrib import messages
 
-
+@login_required
 def taskList(request):
-    tasks_list = Task.objects.all().order_by('-created_at')
 
-    paginator = Paginator(tasks_list, 3)
-    page = request.GET.get('page')
+    search = request.GET.get('search')
+    if search:
+        tasks = Task.objects.filter(title__icontains=search)
+    else:
 
-    tasks = paginator.get_page(page) 
+        tasks_list = Task.objects.all().order_by('-created_at')
+
+        paginator = Paginator(tasks_list, 3)
+        page = request.GET.get('page')
+
+        tasks = paginator.get_page(page) 
 
     return render(request, 'tasks/list.html', {'tasks':tasks})
-
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
-
+@login_required
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -33,10 +40,10 @@ def newTask(request):
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form})
 
-
+@login_required
 def yourName(request, name):
     return render(request, 'tasks/yourname.html', {'name': name})
-
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -49,11 +56,11 @@ def editTask(request, id):
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
 
 
-
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/deletetask.html', {'task': task})
-
+@login_required
 def deleteReallyTask(request, id):
     task = get_object_or_404(Task, pk=id)
     print(request)
